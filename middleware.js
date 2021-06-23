d71967c5debd1ee342169249ed44d9141f1773de
roniceyemeli <roniceyemeli@gmail.com>
2021-06-23 19:46:48 +0100

    updating files

--- middleware.js
+++ middleware.js
@@ -1,13 +1,12 @@
 
 const h = new Date();
 const heure = h.getHours();
+const jour = h.getDay();
 
-const j = new Date();
-const jour = j.getDay();
-
-const workingTime = (res,req,next) =>{
+const workingTime = (req,res,next) =>{
     if(heure < 9 || heure > 17|| jour < 1 || jour > 5){
         console.log(" sorry can't serve your request at this time, try in working hours")
+        res.send(' <h1>Sorry try in working hours </h1> ')
     }
     else{
         console.log("page have been send to the client")
